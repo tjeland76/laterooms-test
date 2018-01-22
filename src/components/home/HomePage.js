6a8ca455d288e0bac49c9061a743ad2425a20e86
tjeland <leedsutd76>
2018-01-22 20:47:26 +0000

    initial commit - shell

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div>
+        home
+      </div>
+    );
+  }
+}
+export default HomePage;
---
+++ tools/srcServer.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import webpack from 'webpack';
+import path from 'path';
+import config from '../webpack.config.dev';
+import open from 'open';
+import bodyParser from 'body-parser';
+/* eslint-disable no-console */
+
+const port = process.env.PORT || 5000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join( __dirname, '../src/index.html'));
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open(`http://localhost:${port}`);
+  }
+});
