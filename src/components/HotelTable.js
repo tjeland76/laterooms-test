d911942a766fd7123c9bda3ffb1b65dbb42d3efe
Trevor Eland <tjeland76@gmail.com>
2018-01-24 20:44:41 +0000

    Refactor and fix failed test for HotelTable - displaying HotelRow

--- src/components/HotelTable.js
+++ src/components/HotelTable.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import HotelRow from './HotelRow';
 
 class HotelTable extends React.Component {
   render() {
 
+    const rows = [];
+
+    this.props.hotels.forEach((hotel, i) => {
+
+      rows.push(
+        <HotelRow
+          hotel={hotel}
+          key={i}
+        />
+      );
+
+    });
 
     return (
-      <div  className="hotelsContainer"/>
+      <div  className="hotelsContainer">
+        {rows}
+      </div>
     );
   }
 }
