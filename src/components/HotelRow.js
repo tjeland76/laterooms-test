7c5558a7bff036f37fab8b6a65405725f28af716
Trevor Eland <tjeland76@gmail.com>
2018-01-24 19:56:53 +0000

    First failed test

---
+++ src/components/HotelRow.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HotelRow = (props) => {
+
+};
+
+export default HotelRow;
--- src/index.test.js
+++ src/index.test.js
@@ -4,16 +4,19 @@ import Adapter from 'enzyme-adapter-react-15';
 
 configure({ adapter: new Adapter() });
 
-import React from 'react'
+import React from 'react';
 import {expect} from 'chai';
-import { shallow, mount } from 'enzyme'
+import { shallow, mount } from 'enzyme';
 
-describe('Our first test', () => {
-  it('should pass', () => {
+import HotelRow from './components/HotelRow'
 
-      expect(true).to.equal(true);
+describe('<HotelRow/>', () => {
+  it('should find hotel name element', () => {
+
+    const row = shallow(<HotelRow hotel={{StarRating:2, Name: 'hotelone', Facilities: ['car park', 'pool']}} />)
+
+    expect(row.find('.hotelName').text()).to.equal('hotelone');
 
   });
-
 
 });
