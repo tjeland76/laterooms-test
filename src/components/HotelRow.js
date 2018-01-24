949372061217c91a4b9986e41bff42b18d99797f
Trevor Eland <tjeland76@gmail.com>
2018-01-24 19:59:10 +0000

    First passed test

--- src/components/HotelRow.js
+++ src/components/HotelRow.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const HotelRow = (props) => {
+  const hotel = props.hotel;
 
+  return (
+    <div>
+      <div className="hotelName">{hotel.Name}</div>
+    </div>
+  );
 };
+
 
 export default HotelRow;
