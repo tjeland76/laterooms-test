c69ba844e75bd111aab4bcdf5a8e99111711f329
Trevor Eland <tjeland76@gmail.com>
2018-01-24 20:10:16 +0000

    Refactor and passing first tests

--- src/components/HotelRow.js
+++ src/components/HotelRow.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 const HotelRow = (props) => {
+
   const hotel = props.hotel;
+  const hotelFacilities = hotel.Facilities.join(', ');
 
   return (
     <div>
       <div className="hotelName">{hotel.Name}</div>
+      <div className="starRating">{hotel.StarRating} Star</div>
+      <div className="facilities">{hotelFacilities}</div>
     </div>
   );
 };
-
 
 export default HotelRow;
--- src/index.test.js
+++ src/index.test.js
@@ -11,11 +11,26 @@ import { shallow, mount } from 'enzyme';
 import HotelRow from './components/HotelRow'
 
 describe('<HotelRow/>', () => {
+  let row;
+  beforeEach(() => {
+    row = shallow(<HotelRow hotel={{StarRating:2, Name: 'hotelone', Facilities: ['car park', 'pool']}} />)
+  });
+
   it('should find hotel name element', () => {
 
-    const row = shallow(<HotelRow hotel={{StarRating:2, Name: 'hotelone', Facilities: ['car park', 'pool']}} />)
-
     expect(row.find('.hotelName').text()).to.equal('hotelone');
+
+  });
+
+  it('should find hotel star rating element', () => {
+
+    expect(row.find('.starRating').text()).to.equal('2 Star');
+
+  });
+
+  it('should find hotel facilities element', () => {
+
+    expect(row.find('.facilities').text()).to.equal('car park, pool');
 
   });
 
